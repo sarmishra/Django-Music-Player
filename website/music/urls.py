a19316be0c07e7be04990de93220f1fb7ca780d7
saroj <saroj.mishra773@gmail.com>
2017-05-29 23:12:56 +0545

    first commit

---
+++ website/music/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name= 'music'
+
+urlpatterns = [
+
+	
+	url(r'^$', views.IndexView.as_view(), name='index'),	
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='datail'),
+
+	#music/album/add/
+	url(r'ablum/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+
+	
+]
+ 
